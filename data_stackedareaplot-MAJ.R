d07bae4140a283c55f670a5b7e779d5be05bea95
Michael A Johansson <majohansson@users.noreply.github.com>
2024-12-05 13:32:31 -0400

    update time index options

--- data_stackedareaplot-MAJ.R
+++ data_stackedareaplot-MAJ.R
@@ -1,18 +1,22 @@
 require(tidyverse)
 
-data_stackedareaplot2 <- function(mats = list(), nsims=NULL, tmax=NULL) {
+data_stackedareaplot2 <- function(mats = list(), nsims=NULL, tmax=NULL, times=NULL) {
   if (!is.matrix(mats[[1]])) {
     for (i in 1:length(mats)) {
       mats[[i]] <- as.matrix(mats[[i]], nrow=length(mats[[i]])) 
     }
   }
   if (is.null(nsims)) nsims <- ncol(mats[[1]])
-  if (is.null(tmax)) tmax <- nrow(mats[[1]])
+  if (is.null(times)) {
+    if (is.null(tmax)) times <- 1:nrow(mats[[1]])
+    else times <- 1:tmax
+  }
+  
   medians <- tibble()
   for (i in 1:length(mats)) {
     medians <- bind_rows(medians, 
       tibble(
-        time = 1:nrow(mats[[1]]),
+        time = times,
         group = names(mats)[i],
         median = apply(mats[[i]], 1, median)))
   }
